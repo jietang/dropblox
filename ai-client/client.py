9ad844e65a0c2e0597db86bcfd29ae830d1fe858
Stephen Poletto <stephenpoletto@gmail.com>
2013-01-24 23:25:25 -0800

    Getting rid of reconnect for testing rounds.

--- ai-client/client.py
+++ ai-client/client.py
@@ -167,7 +167,7 @@ class Subscriber(WebSocketClient):
     def closed(self, code, reason=None):
         print colorred.format("Connection to server closed. Code=%s, Reason=%s" % (code, reason))
 
-        if code != DO_NOT_RECONNECT and 'entry_mode' == 'compete':
+        if code != DO_NOT_RECONNECT and entry_mode == 'compete':
             # Attempt to re-connect
             ws = Subscriber(WEBSOCKET_URL)
             ws.connect()
