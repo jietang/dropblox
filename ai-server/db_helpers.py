c57ffd09f5713fbfe2b08df4b2bb9618554bdc2e
Stephen Poletto <stephenpoletto@gmail.com>
2013-01-31 23:47:50 -0800

    Add database helpers.

--- ai-server/db_helpers.py
+++ ai-server/db_helpers.py
@@ -26,7 +26,7 @@ class DatabaseHelpers(object):
 		conn = mdb.connect(host='localhost', user='dropblox', passwd='dropblox', db='dropblox')
 		sql = 'DROP DATABASE dropblox;'
 		cursor = conn.cursor()
-		cursor.execute(sql, (team_name, password, 0))
+		cursor.execute()
 		conn.commit()
 
 if __name__ == '__main__':
